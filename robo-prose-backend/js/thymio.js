e1e810b7e71309b111e2d486990ec5f45fb913b4
Davide Laezza <truzzialrogo@gmx.com>
2017-11-14 14:55:22 +0100

    Added DBus implementation. Not tested, though

--- robo-prose-backend/js/thymio.js
+++ robo-prose-backend/js/thymio.js
@@ -2,8 +2,35 @@
  *
  */
 
-class ThymioDBusObject {
+const dbus = require('dbus-native');
+const q = require('q');
 
+class ThymioDBusObject {
+    constructor(name) {
+        this.name = name;
+
+        this.bus = dbus.sessionBus();
+        this.network = q.ninvoke(this.bus, 'getInterface',
+                'ch.epfl.mobots.Aseba',
+                '/',
+                'ch.epfl.mobots.AsebaNetwork');
+    }
+
+    networkCall(method, ...args) {
+        return this.network.then(network =>
+            q.ninvoke(network, method, this.name, ...args))
+    }
+
+    get(variable) {
+        return this.networkCall('GetVariable', variable)
+            .then(value => Array.isArray(value)
+                ? value.map(parseInt)
+                : parseInt(value));
+    }
+
+    set(variable, value) {
+        return this.networkCall('SetVariable', variable, value);
+    }
 }
 
 class Thymio extends ThymioDBusObject {
@@ -15,11 +42,27 @@ class Thymio extends ThymioDBusObject {
     }
 
     constructor(main, listeners) {
-        super();
+        super('thymio-II');
         this.main = main;
         if (listeners) {
             listeners.forEach(this.addListener.bind(this));
         }
+    }
+
+    move(speed) {
+        const arg = [speed];
+        return q.all([
+            this.set('motor.left.target', arg),
+            this.set('motor.right.target', arg)
+        ]);
+    }
+
+    moveBackward() {
+        return this.move(200);
+    }
+
+    moveForward() {
+        return this.move(-200);
     }
 
     run() {
@@ -27,7 +70,12 @@ class Thymio extends ThymioDBusObject {
     }
 
     runAction(action) {
-        this[action.method].apply(this, action.args);
+        this[action.method].apply(this, action.args)
+            .done();
+    }
+
+    stop() {
+        return this.move(0);
     }
 }
 
