052259aa6d7033afc5ecd91761966eb51da21d04
Davide Laezza <truzzialrogo@gmx.com>
2017-11-21 14:33:33 +0100

    Why weren't these mesged?

---
+++ robo-prose-model/src/dk/itu/dsl/roboprose/model/Stop.java
@@ -0,0 +1,17 @@
+/**
+ */
+package dk.itu.dsl.roboprose.model;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Stop</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see dk.itu.dsl.roboprose.model.RoboprosePackage#getStop()
+ * @model
+ * @generated
+ */
+public interface Stop extends Action, hasDuration {
+} // Stop
---
+++ robo-prose-model/src/dk/itu/dsl/roboprose/model/Tapped.java
@@ -0,0 +1,17 @@
+/**
+ */
+package dk.itu.dsl.roboprose.model;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Tapped</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see dk.itu.dsl.roboprose.model.RoboprosePackage#getTapped()
+ * @model
+ * @generated
+ */
+public interface Tapped extends Event {
+} // Tapped
