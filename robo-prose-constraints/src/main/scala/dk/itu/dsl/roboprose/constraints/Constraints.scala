3225acdbb17361d07fe2fe19ec3ce17137175ddf
Richard <rbanyi@me.com>
2017-11-28 12:30:01 +0100

    new constraints

--- robo-prose-constraints/src/main/scala/dk/itu/dsl/roboprose/constraints/Constraints.scala
+++ robo-prose-constraints/src/main/scala/dk/itu/dsl/roboprose/constraints/Constraints.scala
@@ -7,11 +7,11 @@ import mdsebook.scala.EMFScala._
 import dk.itu.dsl.roboprose.model._
 
 object Constraints {
-  
+
   def has[A] (a : => A) = Option(a).nonEmpty
-  
+
   val invariants: Map[String, Constraint] = Map (
-      
+
       "Root must have at least one robot." -> inv[Root] {
           self => self.getRobots != null },
 
@@ -22,6 +22,30 @@ object Constraints {
         self => !self.getMain.getActions.isEmpty },
 
       "RoboProse may have eventListener" -> inv[RoboProse] {
-        self => self.getListeners == null }
+        self => self.getListeners != null },
+
+      "RoboProse with eventListener must have an event attached"
+        -> inv[EventListener] { self =>
+          self.getEvent != null
+        },
+
+      "Event can be type of Obstacle or Tapped"
+        -> inv[RoboProse] { self =>
+          self.getListeners.forall { l =>
+            l.getEvent.isInstanceOf[Obstacle] ||
+              l.getEvent.isInstanceOf[Tapped]
+          }
+        },
+
+      "Acttion can be type of Move, Turn or Stop" -> inv[Main] {
+        self => self.getActions.forall { a =>
+          a.isInstanceOf[Move] || a.isInstanceOf[Turn] ||
+          a.isInstanceOf[Stop]
+        }
+      },
+
+      "Acttion can be type of Move, Turn or Stop" -> inv[Move] {
+        self => self.getDirection.getValue
+      },
   )
-}   
+}
